84c4dd43fa73524e59b529a6a2b9edc7ff9dc5c0
deidg <64797316+deidg@users.noreply.github.com>
2024-05-29 21:04:07 +0300

    Initial Commit

---
+++ AlexBeerShop_MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlexBeerShop_MVVM
+//
+//  Created by Alex on 29.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
