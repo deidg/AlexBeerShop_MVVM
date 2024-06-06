765c0a9f0ef2b0a75ef9e77f7a881bd1a8336078
deidg <64797316+deidg@users.noreply.github.com>
2024-06-06 19:47:43 +0300

    init commit

---
+++ AlexBeerShop_MVVM/Models/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  AlexBeerShop_MVVM
+//
+//  Created by Alex on 06.06.2024.
+//
+
+import Foundation
---
+++ AlexBeerShop_MVVM/Models/Networking/NetworkRequest.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkRequest.swift
+//  AlexBeerShop_MVVM
+//
+//  Created by Alex on 06.06.2024.
+//
+
+import Foundation
